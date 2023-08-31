47c130cbec3aafd48274774a3a3591f8a6d32a6b
Jacky Chan <jackycct@gmail.com>
2023-09-01 00:15:42 +0100

    add off track

--- 2023-london-ace-speedway/jc-local-center-progress/reward_function.py
+++ 2023-london-ace-speedway/jc-local-center-progress/reward_function.py
@@ -6,18 +6,24 @@ def reward_function(params):
     distance_from_center = params['distance_from_center']
     progress = params['progress']
     steps = params['steps']
+    is_offtrack = params['is_offtrack']
 
-    raceline_reward = 1e-3
-    if distance_from_center >= 0.0 and distance_from_center <= 0.02:
-        raceline_reward = 1.0
-    elif distance_from_center >= 0.02 and distance_from_center <= 0.03:
-        raceline_reward = 0.3
-    elif distance_from_center >= 0.03 and distance_from_center <= 0.05:
-        raceline_reward = 0.1
-    
-    progress_reward = progress / steps
+    if is_offtrack:
+        reward = 1e-3
+    else:
+        raceline_reward = 1e-3
+        if distance_from_center >= 0.0 and distance_from_center <= 0.02:
+            raceline_reward = 1.0
+        elif distance_from_center >= 0.02 and distance_from_center <= 0.03:
+            raceline_reward = 0.3
+        elif distance_from_center >= 0.03 and distance_from_center <= 0.05:
+            raceline_reward = 0.1
+        
+        progress_reward = progress / steps
 
-    print("raceline_reward = " + format(raceline_reward, ".3f"))
-    print("progress_reward = " + format(progress_reward, ".3f"))
+        print("raceline_reward = " + format(raceline_reward, ".3f"))
+        print("progress_reward = " + format(progress_reward, ".3f"))
 
-    return progress_reward + raceline_reward ** 2
+        reward = progress_reward + raceline_reward ** 2
+
+    return reward
