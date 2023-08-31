4d9e96446428fb176910eb01fc35a29f04f7af3a
Jacky Chan <jackycct@gmail.com>
2023-08-31 23:44:27 +0100

    create simple starting reward function

---
+++ 2023-london-ace-speedway/jc-local-center-progress/reward_function.py
@@ -0,0 +1,23 @@
+from numpy import array
+import math
+
+def reward_function(params):
+    # Read input parameters
+    distance_from_center = params['distance_from_center']
+    progress = params['progress']
+    steps = params['steps']
+
+    raceline_reward = 1e-3
+    if distance_from_center >= 0.0 and distance_from_center <= 0.02:
+        raceline_reward = 1.0
+    elif distance_from_center >= 0.02 and distance_from_center <= 0.03:
+        raceline_reward = 0.3
+    elif distance_from_center >= 0.03 and distance_from_center <= 0.05:
+        raceline_reward = 0.1
+    
+    progress_reward = progress / steps
+
+    print("raceline_reward = " + format(raceline_reward, ".3f"))
+    print("progress_reward = " + format(progress_reward, ".3f"))
+
+    return progress_reward + raceline_reward ** 2
