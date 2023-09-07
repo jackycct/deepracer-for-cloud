ad19f9baabe379d793bff3ba4c81b26a1caf8669
Jacky Chan <jackycct@gmail.com>
2023-09-07 13:34:46 +0100

    fine tune speed

--- 2023-london-ace-speedway/jc-local-raceline-4751/reward_function.py
+++ 2023-london-ace-speedway/jc-local-raceline-4751/reward_function.py
@@ -314,14 +314,12 @@ class Reward:
             # if 25 <= wp <= 28:
                 # speed_reward = calculate_reward_using_signmoid(abs(speed - 2.6), 5)             
             if 42 <= wp <= 49:    
-                speed_reward = calculate_reward_using_signmoid(abs(speed - 2.0), 5)
-            # elif 46 <= wp <= 56:
-            #     speed_reward = calculate_reward_using_signmoid(abs(speed - 2.5), 5)
+                speed_reward = calculate_reward_using_signmoid(abs(speed - 2.4), 5)
             elif 85 <= wp <= 90:
                 speed_reward = calculate_reward_using_signmoid(abs(speed - 3.1), 5)
-            elif 98 <= wp <= 102:
-                speed_reward = calculate_reward_using_signmoid(abs(speed - 2.0), 5)
-            elif 112 <= wp <= 116:
+            elif 91 <= wp <= 102:
+                speed_reward = calculate_reward_using_signmoid(abs(speed - 2.4), 5)
+            elif 102 <= wp <= 128:
                 speed_reward = calculate_reward_using_signmoid(abs(speed - 3.6), 5)
             elif wp > 135 or wp < 10:
                 speed_reward = calculate_reward_using_signmoid(abs(speed - 4.0), 5)
--- 2023-london-ace-speedway/jc-local-raceline-4751/training-steps.md
+++ 2023-london-ace-speedway/jc-local-raceline-4751/training-steps.md
@@ -1,3 +1,4 @@
 1. Use the current hyperparameters to train until 40 training iteration. 
-1. Reduce the lr to 0.00003 and increase num_epochs to 20 to fine tune the model
+1. Reduce the lr to 0.000003 and increase num_epochs to 20 to fine tune the model
+1. Reduce the lr to 0.0000003 to fine tune the model
 1. Improve the speed reward
